d0ef14cefcfa797460cb0322b2bda5e1ea4cb4e8
Juan Jose <carrerotalora15@gmail.com>
2024-07-17 14:32:46 -0500

    Agrega conversion de dato a entero

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,13 +1,12 @@
-#print(input("Inserte su nombre: "))
-#print(input("Inserte su edad: "))
-#print(input("Inserte su ID: "))
-#print("Bienvenido señor "  + input("Inserte su nombre: ") + "" + " su edad es " +  input("Inserte su edad: "))
-#print(input("Inserte su ID: "))
-nombre = input("inserte su nombre: ")
-edad = input("Inserte su edad: ")
-ID = input("Inserte su ID: ")
-frase = print(f"Hola {nombre} su edad es {edad} y su ID es {ID}")
 
+#nombre = input("inserte su nombre: ")
+#edad = input("Inserte su edad: ")
+#identificacion = input("Inserte su ID: ")
+#frase = print(f"Hola {nombre} su edad es {edad} y su ID es {identificacion}")
+identificacion = input("Ingrese su numero de identificacion: ")
+print(type(identificacion))
+identidicacion2 = int(identificacion)
+print(type(identidicacion2))
 
 
 
