c4117a786c73647e3ac6a896e2d8cd97d91831df
Juan Jose <carrerotalora15@gmail.com>
2024-07-17 14:01:26 -0500

    pregunta de edad y nombre

---
+++ Ejercicio.py
@@ -0,0 +1,6 @@
+#print(input("Inserte su nombre: "))
+#print(input("Inserte su edad: "))
+#print(input("Inserte su ID: "))
+print("Bienvenido señor "  + input("Inserte su nombre: ") + "" + " Su edad es " +  input("Inserte su edad: "))
+
+
