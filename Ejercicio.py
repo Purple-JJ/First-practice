e82ea0047adf3bfcf121bb74c3dc9993bdb69579
Juan Jose <carrerotalora15@gmail.com>
2024-07-17 14:16:12 -0500

    pregunta nombre,edad y numero

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,6 +1,13 @@
 #print(input("Inserte su nombre: "))
 #print(input("Inserte su edad: "))
 #print(input("Inserte su ID: "))
-print("Bienvenido señor "  + input("Inserte su nombre: ") + "" + " Su edad es " +  input("Inserte su edad: "))
+#print("Bienvenido señor "  + input("Inserte su nombre: ") + "" + " su edad es " +  input("Inserte su edad: "))
+#print(input("Inserte su ID: "))
+nombre = input("inserte su nombre: ")
+edad = input("Inserte su edad: ")
+ID = input("Inserte su ID: ")
+frase = print(f"Hola {nombre} su edad es {edad} y su ID es {ID}")
+
+
 
 
